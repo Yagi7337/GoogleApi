51b1d4fb7c9d435eeafeed9fc7fdac301433dbe2
Yagi7337 <yagi7337@gmail.com>
2017-02-09 16:45:39 +0200

    Create and delete file in Google Drive #3

--- src/main/java/com/api/google/service/GoogleDriveService.java
+++ src/main/java/com/api/google/service/GoogleDriveService.java
@@ -1,5 +1,14 @@
 package com.api.google.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.api.google.component.GoogleAuthorization;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -7,36 +16,22 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.*;
 import com.google.api.services.drive.Drive;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.List;
-
-import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.ParentReference;
-
-import org.springframework.stereotype.Service;
+import com.google.api.services.drive.model.FileList;
 
 @Service
-public class GoogleDriveService {
+public class GoogleDriveService extends GoogleAuthorization {
 	/** Application name. */
-	private static final String APPLICATION_NAME = "TryGoogle";
+	private static final String APPLICATION_NAME = "GoogleApi";
 
 	/** Directory to store user credentials for this application. */
 	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
-			".credentials/TryGoogle");
+			".credentials/GoogleApi");
 
 	/** Global instance of the {@link FileDataStoreFactory}. */
 	private static FileDataStoreFactory DATA_STORE_FACTORY;
@@ -48,12 +43,12 @@ public class GoogleDriveService {
 	private static HttpTransport HTTP_TRANSPORT;
 
 	/**
-	 * Global instance of the scopes required by this quickstart.
+	 * Global instance of the scopes required by this GoogleApi.
 	 *
 	 * If modifying these scopes, delete your previously saved credentials at
-	 * ~/.credentials/drive-java-quickstart
+	 * ~/.credentials/GoogleApi
 	 */
-	private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+	private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
 
 	static {
 		try {
@@ -77,9 +72,12 @@ public class GoogleDriveService {
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
 		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder
+				(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+				.setDataStoreFactory(DATA_STORE_FACTORY)
+				.setAccessType("offline").build();
+		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver())
+				.authorize("user");
 		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
 		return credential;
 	}
@@ -92,10 +90,12 @@ public class GoogleDriveService {
 	 */
 	public static Drive getDriveService() throws IOException {
 		Credential credential = authorize();
-		return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
+		return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+				.setApplicationName(APPLICATION_NAME)
+				.build();
 	}
 
-	public void StartApp() throws IOException {
+	public void showFiles() throws IOException {
 		// Build a new authorized API client service.
 		Drive service = getDriveService();
 
@@ -112,80 +112,26 @@ public class GoogleDriveService {
 		}
 	}
 
-	// public void StartApp() throws IOException{
-	// Drive service = getDriveService();
-	// File createFile = new File();
-	// createFile.setTitle("Hello");
-	// File file = service.files().insert(createFile ).execute();
-	// System.out.println("File ID: " + file.getId());
-	//
-	// }
+	public void createGoogleDocFile() throws IOException {
+		Drive service = getDriveService();
+		
+		String mimeType = "application/vnd.google-apps.document";
+		String description = "my first test with google docs";
 
-//	 public void StartApp() throws IOException {
-//	 Drive service = getDriveService();;
-//	 String fileId = "1yvsgdIqeD03pHvypCKeHPePZARWOtwWPwVIR6qHkOVk";
-//	 service.files().delete(fileId).execute();
-//	 }
+		File createdFile = new File();
+		createdFile.setTitle("Hey. Second test");
+		createdFile.setMimeType(mimeType);
+		createdFile.setDescription(description);
+		
+		File file = service.files().insert(createdFile).execute();
+		System.out.println("File ID: " + file.getId());
 
-	// private static void deleteFile(Drive service, ) {
-	// try {
-	// service.files().delete(fileId).execute();
-	// } catch (IOException e) {
-	// System.out.println("An error occurred: " + e);
-	// }
-	// }
+	}
 
-	// /**
-	// * Insert new file.
-	// *
-	// * @param service
-	// * Drive API service instance.
-	// * @param title
-	// * Title of the file to insert, including the extension.
-	// * @param description
-	// * Description of the file to insert.
-	// * @param parentId
-	// * Optional parent folder's ID.
-	// * @param mimeType
-	// * MIME type of the file to insert.
-	// * @param filename
-	// * Filename of the file to insert.
-	// * @return Inserted file metadata if successful, {@code null} otherwise.
-	// * @throws IOException
-	// */
-	// public static File StartApp() throws IOException {
-	// Drive service = getDriveService();
-	//
-	// String title = "Test project";
-	// String description = "one_two_three";
-	// String parentId = "0AMHoJS2OBV_rUk9PVA";
-	// String mimeType = "application/vnd.google-apps.document";
-	// String filename = "test";
-	//
-	// // File's metadata.
-	// File body = new File();
-	// body.setTitle(title);
-	// body.setDescription(description);
-	// body.setMimeType(mimeType);
-	//
-	// // Set the parent folder.
-	// if (parentId != null && parentId.length() > 0) {
-	// body.setParents(Arrays.asList(new ParentReference().setId(parentId)));
-	// }
-	//
-	// // File's content.
-	// java.io.File fileContent = new java.io.File(filename);
-	// FileContent mediaContent = new FileContent(mimeType, fileContent);
-	// try {
-	// File file = service.files().insert(body, mediaContent).execute();
-	//
-	// // Uncomment the following line to print the File ID.
-	// // System.out.println("File ID: " + file.getId());
-	//
-	// return file;
-	// } catch (IOException e) {
-	// System.out.println("An error occured: " + e);
-	// return null;
-	// }
-	// }
+	public void deleteFile() throws IOException {
+		Drive service = getDriveService();
+		String fileId = "1yvsgdIqeD03pHvypCKeHPePZARWOtwWPwVIR6qHkOVk";
+		service.files().delete(fileId).execute();
+	}
+
 }
